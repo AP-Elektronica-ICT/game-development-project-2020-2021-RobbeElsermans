75653dc7b3e58ea00d08e84828df6c8064ef38f1
Robbe Elsermans <robels@outlook.com>
2020-10-19 16:10:26 +0200

    werkend mannetje + attack + 2side walk

--- Pigit/Pigit/Human.cs
+++ Pigit/Pigit/Human.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using SharpDX.MediaFoundation;
+using System;
 using System.Linq.Expressions;
 
 namespace Pigit
@@ -11,7 +12,8 @@ namespace Pigit
     {
         private Texture2D heroTextureR;
         private Texture2D heroTextureL;
-        Animatie animatie;
+        Animatie animatieR;
+        Animatie animatieL;
         static Vector2 positie;
         Vector2 snelheid;
         Vector2 versnelling;
@@ -27,18 +29,23 @@ namespace Pigit
             }
         }
 
-
         public Human(Texture2D textureRight, Texture2D textureLeft, Vector2 size, int amountFrames)
         {
             
             this.heroTextureR = textureRight;
             this.heroTextureL = textureLeft;
-            animatie = new Animatie();
+            animatieR = new Animatie();
+            animatieL = new Animatie();
             Animatie.Speed = 8;
 
             for (int i = 0; i <= size.X * amountFrames -1; i += (int)size.X)
             {
-                animatie.AddFrame(new AnimatieFrame(new Rectangle(i, 0, (int)size.X, (int)size.Y)));
+                animatieR.AddFrame(new AnimatieFrame(new Rectangle(i, 0, (int)size.X, (int)size.Y)));
+            }
+
+            for (int i = Convert.ToInt32(size.X * (amountFrames-1)); i >= 0; i -= (int)size.X)
+            {
+                animatieL.AddFrame(new AnimatieFrame(new Rectangle(i, 0, (int)size.X, (int)size.Y)));
             }
 
             snelheid = new Vector2(1, 1);
@@ -48,22 +55,24 @@ namespace Pigit
         {
             if (Direction)
             {
-                _spriteBatch.Draw(heroTextureL, positie, animatie.CurrentFrame.SourceRect, Color.White);
+                _spriteBatch.Draw(heroTextureL, positie, animatieL.CurrentFrame.SourceRect, Color.White);
             }
             else
             {
-                _spriteBatch.Draw(heroTextureR, positie, animatie.CurrentFrame.SourceRect, Color.White);
+                _spriteBatch.Draw(heroTextureR, positie, animatieR.CurrentFrame.SourceRect, Color.White);
             }
         }
 
         public void Update(GameTime gameTime, Vector2 verplaatsing)
         {
             this.Move(verplaatsing);
-            animatie.Update(gameTime);
+            animatieR.Update(gameTime);
+            animatieL.Update(gameTime);
         }
         public void Update(GameTime gameTime)
         {
-            animatie.Update(gameTime);
+            animatieR.Update(gameTime);
+            animatieL.Update(gameTime);
         }
 
         private void Move(Vector2 verplaatsing)
