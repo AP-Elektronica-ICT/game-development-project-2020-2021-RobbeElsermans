0255b51b4a807abb572cf69c6ca7a95a7d623a2d
Robbe Elsermans <robels@outlook.com>
2020-12-29 15:44:15 +0100

    refactor AttackCommand

--- Pigit/Pigit/Attack/AttackCommand.cs
+++ Pigit/Pigit/Attack/AttackCommand.cs
@@ -13,21 +13,36 @@ namespace Pigit.Attack
     {
         private double timer;
         private bool isSetTimer = false;
+        private static float attackTicksHero = 0.5f;
+        private static float attackTicksEnemy = 0.7f;
 
         public void Attack(List<AEnemyObject> enemys, IPlayerObject player, GameTime gametime)
         {
-            //Attack an enemy
+            SetTimer(gametime);
+            CheckCollision(enemys, player, gametime, attackTicksHero);
+        }
+        public void NPCAttack(IPlayerObject enemy, AEnemyObject player, GameTime gametime)
+        {
+            SetTimer(gametime);
+            CheckCollision(new List<AEnemyObject> { player }, enemy, gametime, attackTicksEnemy);
+        }
+
+        private void SetTimer(GameTime gametime)
+        {
             if (!isSetTimer)
             {
                 timer = gametime.TotalGameTime.TotalSeconds;
                 isSetTimer = true;
             }
+        }
 
+        private void CheckCollision(List<AEnemyObject> enemys, IPlayerObject player, GameTime gametime, float ticks)
+        {
             foreach (var enemy in enemys)
             {
                 if (NPCCollision.IsTouchingNPC(player.Rectangle, enemy.Rectangle))
                 {
-                    if (gametime.TotalGameTime.TotalSeconds - timer > 0.5)
+                    if (gametime.TotalGameTime.TotalSeconds - timer > ticks)
                     {
                         isSetTimer = false;
                         enemy.Hearts -= player.AttackDamage;
@@ -35,28 +50,6 @@ namespace Pigit.Attack
                         {
                             enemy.IsHit = true;
                         }
-                    }
-                }
-            }
-        }
-        public void NPCAttack(IPlayerObject enemy, AEnemyObject player, GameTime gametime)
-        {
-            //Attack an enemy
-            if (!isSetTimer)
-            {
-                timer = gametime.TotalGameTime.TotalSeconds;
-                isSetTimer = true;
-            }
-
-            if (NPCCollision.IsTouchingNPC(player.Rectangle, enemy.Rectangle))
-            {
-                if (gametime.TotalGameTime.TotalSeconds - timer > 0.7)
-                {
-                    isSetTimer = false;
-                    enemy.Hearts -= player.AttackDamage;
-                    if (!enemy.IsHit)
-                    {
-                        enemy.IsHit = true;
                     }
                 }
             }
